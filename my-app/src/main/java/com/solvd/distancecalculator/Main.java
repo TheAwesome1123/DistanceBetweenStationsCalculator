6e943152ee0c4f3cf3904cde4a32facd2dff1849
TheAwesome1123 <nguyens22@up.edu>
2023-07-07 16:01:36 -0700

    Database is now 0-indexed; did work about intermediate stations in path.

--- my-app/src/main/java/com/solvd/distancecalculator/Main.java
+++ my-app/src/main/java/com/solvd/distancecalculator/Main.java
@@ -2,6 +2,7 @@ package com.solvd.distancecalculator;
 
 import com.solvd.distancecalculator.daos.PathBetweenStationsDAO;
 import com.solvd.distancecalculator.models.PathBetweenStations;
+import com.solvd.distancecalculator.models.Station;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.List;
@@ -13,24 +14,28 @@ public class Main {
         try {
             int startingStationID = Integer.parseInt(args[0]);
             int endingStationID = Integer.parseInt(args[1]);
-            LOGGER.info("The shortest distance between stations " + startingStationID + " and " + endingStationID
-                + " is...");
 
-            int[][] distances = FloydAlgorithm.getMatrix(10);
-
-            // Each entry represents station in shortest route from start (row) to end (col).
-            int[][] shortestPaths = FloydAlgorithm.getMatrix(10);
+            Integer[][] distances = FloydAlgorithm.getMatrix(10);
+            Integer[][] stationsInShortestPath = FloydAlgorithm.getMatrix(10);
 
             List<PathBetweenStations> list = new PathBetweenStationsDAO().getPaths();
             LOGGER.info("Number of roads: " + list.size());
 
-            // Primarily for testing; feel free to delete if unnecessary.
+            // Adding and checking initial distances.
             FloydAlgorithm.addInitialDistances(distances, list);
+            FloydAlgorithm.addInitialNodesInShortestDistance(distances, stationsInShortestPath);
             FloydAlgorithm.printDistances(distances);
+            FloydAlgorithm.printDistances(stationsInShortestPath);
 
             // Insert Floyd's Algorithm implementation here.
+            LOGGER.info("The shortest distance between stations " + startingStationID + " and " + endingStationID
+                    + " is...");
 
             // Insert marshalling here.
+            List<Station> finalStations =
+                FloydAlgorithm.getStationsInShortestPath(stationsInShortestPath, 1, 2);
+
+            LOGGER.info("hi");
         }
         catch(NumberFormatException numberFormatException) {
             LOGGER.info(numberFormatException);
