03bd18bb55b7a24ad252879f59f2a9c3d56ed1f0
TheAwesome1123 <nguyens22@up.edu>
2023-07-11 16:31:00 -0700

    XML work.

--- my-app/src/main/java/com/solvd/distancecalculator/Main.java
+++ my-app/src/main/java/com/solvd/distancecalculator/Main.java
@@ -1,15 +1,19 @@
 package com.solvd.distancecalculator;
 
 import com.solvd.distancecalculator.daos.PathBetweenStationsDAO;
-import com.solvd.distancecalculator.models.PathBetweenStations;
-import com.solvd.distancecalculator.models.Station;
+import com.solvd.distancecalculator.daos.RoadDAO;
+import com.solvd.distancecalculator.models.*;
+import com.solvd.distancecalculator.xml.ShortestPathMarshaller;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     private static final Logger LOGGER = LogManager.getLogger(Main.class);
     private static final PathBetweenStationsDAO PATH_BETWEEN_STATIONS_DAO = new PathBetweenStationsDAO();
+    private static final RoadDAO ROAD_DAO = new RoadDAO();
 
     public static void main(String[] args) {
         try {
@@ -20,7 +24,7 @@ public class Main {
             Integer[][] distances = FloydAlgorithm.getMatrix(numOfPaths);
             Integer[][] stationsInShortestPath = FloydAlgorithm.getMatrix(numOfPaths);
 
-            List<PathBetweenStations> list = new PathBetweenStationsDAO().getPaths();
+            List<PathBetweenStations> list = PATH_BETWEEN_STATIONS_DAO.getPaths();
             LOGGER.info("Number of roads: " + numOfPaths);
 
             LOGGER.info("Calculating shortest distance between stations " + startingStationID + " and " +
@@ -29,19 +33,41 @@ public class Main {
             // Adding and checking initial distances.
             FloydAlgorithm.addInitialDistances(distances, list);
             FloydAlgorithm.addInitialNodesInShortestDistance(distances, stationsInShortestPath);
-            FloydAlgorithm.printDistances(distances);
-            FloydAlgorithm.printDistances(stationsInShortestPath);
+
 
             // Insert Floyd's Algorithm implementation here.
 
 
-            // Insert marshalling here.
-            List<Station> finalStations =
-                FloydAlgorithm.getStationsInShortestPath(stationsInShortestPath, startingStationID, endingStationID);
+            // Setting up ShortestPath for marshalling.
+            Stations stations = new Stations();
+            stations.setStationList(FloydAlgorithm.
+                getStationsInShortestPath(stationsInShortestPath, startingStationID, endingStationID));
 
-            for(Station station : finalStations) {
-                LOGGER.info(station.getID() + " " + station.getName());
+            ShortestPath shortestPath = new ShortestPath();
+            shortestPath.setStations(stations);
+            shortestPath.setDistance(distances[startingStationID][endingStationID]);
+
+            // To get the roads, get roads where the connected stations are two next to each other in list.
+            Roads roads = new Roads();
+            List<Road> roadList = new ArrayList<>();
+            List<Station> stationList = stations.getStationList();
+
+            for(int i = 0; i < stationList.size() - 1; i++) {
+                // Make PathBetweenStations using current stations, and use its start/end IDs to select Road.
+                Station start = stationList.get(i);
+                Station end = stationList.get(i + 1);
+                PathBetweenStations path = new PathBetweenStations();
+
+                path.setStartingStationID(start.getID());
+                path.setEndingStationID(end.getID());
+                Road road = ROAD_DAO.getRoadGivenStartAndEnd(path);
+                roadList.add(road);
             }
+
+            roads.setRoadList(roadList);
+            shortestPath.setRoads(roads);
+
+            ShortestPathMarshaller.marshallXML(shortestPath);
         }
         catch(NumberFormatException numberFormatException) {
             LOGGER.info(numberFormatException);
